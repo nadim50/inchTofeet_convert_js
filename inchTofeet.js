dbe0ebde8e66ba82b8ba85983f16067f21a10417
nadim50 <mdnadimmir50@gmail.com>
2022-08-06 04:03:29 +0600

    first commit

---
+++ inchTofeet.js
@@ -0,0 +1,15 @@
+
+/// declare function
+
+function inchTofeet(inch) {
+    let feet = inch / 12;
+    return feet;
+}
+
+let inch = 144;
+
+/// function called 
+
+let feet = inchTofeet(inch);
+
+console.log('The inch converting feet  is : ', feet);
